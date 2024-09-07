0806a0c51649fe7095d82c3a486f711174973db3
SuperSteve9 <80017099+SuperSteve9@users.noreply.github.com>
2024-09-06 22:01:27 -0500

    Update downloader.bat

--- downloader.bat
+++ downloader.bat
@@ -3,6 +3,6 @@ if %1==rd-132211 goto :132211
 
 :132211
 cd versions
-curl -OL https://github.com/SuperSteve9/spartancraft/raw/main/versions/rd-132211.jar
+curl -OL https://github.com/SuperSteve9/spartancraft/raw/main/versions/minecraft-rd-132211.jar
 cd ..
-"C:\Program Files\Java\jdk-14.0.2\bin\java.exe" -Dorg.lwjgl.librarypath=%cd%\natives -jar %cd%\versions\rd-132211.jar
+"C:\Program Files\Java\jdk-14.0.2\bin\java.exe" -Dorg.lwjgl.librarypath=%cd%\natives -jar %cd%\versions\minecraft-rd-132211.jar
