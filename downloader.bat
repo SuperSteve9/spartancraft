3d88baa8eed19c9a3d866b72a4006126de5ad0ba
SuperSteve9 <80017099+SuperSteve9@users.noreply.github.com>
2024-09-06 22:36:30 -0500

    Update downloader.bat

--- downloader.bat
+++ downloader.bat
@@ -7,36 +7,26 @@ if %1=="SpartanCraft 11a" goto 11a
 
 
 :132211
-cd versions
 curl -OL https://github.com/SuperSteve9/spartancraft/raw/main/versions/minecraft-rd-132211.jar
-cd ..
-"C:\Program Files\Java\jdk-14.0.2\bin\java.exe" -Dorg.lwjgl.librarypath=%cd%\natives -jar %cd%\versions\minecraft-rd-132211.jar
+"C:\Program Files\Java\jdk-14.0.2\bin\java.exe" -Dorg.lwjgl.librarypath=%cd%\natives -jar %cd%\minecraft-rd-132211.jar
 exit
 
 :132328
-cd versions
 curl -OL https://github.com/SuperSteve9/spartancraft/raw/main/versions/minecraft-rd-132328.jar
-cd ..
-"C:\Program Files\Java\jdk-14.0.2\bin\java.exe" -Dorg.lwjgl.librarypath=%cd%\natives -jar %cd%\versions\minecraft-rd-132328.jar
+"C:\Program Files\Java\jdk-14.0.2\bin\java.exe" -Dorg.lwjgl.librarypath=%cd%\natives -jar %cd%\minecraft-rd-132328.jar
 exit
 
 :160052
-cd versions
 curl -OL https://github.com/SuperSteve9/spartancraft/raw/main/versions/minecraft-rd-160052.jar
-cd ..
-"C:\Program Files\Java\jdk-14.0.2\bin\java.exe" -Dorg.lwjgl.librarypath=%cd%\natives -jar %cd%\versions\minecraft-rd-160052.jar
+"C:\Program Files\Java\jdk-14.0.2\bin\java.exe" -Dorg.lwjgl.librarypath=%cd%\natives -jar %cd%\minecraft-rd-160052.jar
 exit
 
 :161348
-cd versions
 curl -OL https://github.com/SuperSteve9/spartancraft/raw/main/versions/minecraft-rd-161348.jar
-cd ..
-"C:\Program Files\Java\jdk-14.0.2\bin\java.exe" -Dorg.lwjgl.librarypath=%cd%\natives -jar %cd%\versions\minecraft-rd-161348.jar
+"C:\Program Files\Java\jdk-14.0.2\bin\java.exe" -Dorg.lwjgl.librarypath=%cd%\natives -jar %cd%\minecraft-rd-161348.jar
 exit
 
 :11a
-cd versions
 curl -OL https://github.com/SuperSteve9/spartancraft/raw/main/versions/minecraft-c0.0.11a.jar
-cd ..
-"C:\Program Files\Java\jdk-14.0.2\bin\java.exe" -Dorg.lwjgl.librarypath=%cd%\natives -jar %cd%\versions\minecraft-c0.0.11a.jar
+"C:\Program Files\Java\jdk-14.0.2\bin\java.exe" -Dorg.lwjgl.librarypath=%cd%\natives -jar %cd%\minecraft-c0.0.11a.jar
 exit
